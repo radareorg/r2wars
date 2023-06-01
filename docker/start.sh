037af8d1ac7f26c184b06d2e6eb76e4211886602
Florian Hofhammer <florian.hofhammer@fhofhammer.de>
2023-06-01 13:29:17 +0200

    Use multi-stage Docker builder and Docker compose
    The multi-stage Docker builder makes better use of caching and
    builds r2 and r2wars independently.
    Docker compose allows easy building ("docker compose build") and
    running ("docker compose up").

--- docker/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# The path to the directory of this file
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
-docker run --rm -it -p 9664:9664 -p 9966:9966 --name r2wars -v $DIR/warriors:/opt/r2wars/csharp/bin/Release/warriors r2wars
